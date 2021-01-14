1209fa4b7f4f5c464f0f0769ed3f708f553f4f36
Marko1212 <shihonage7@gmail.com>
2021-01-14 19:00:21 +0300

    update

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -8,28 +8,44 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PropertyController extends AbstractController
 {
+
+private $properties = [
+['title' => 'Maison avec piscine'],
+['title' => 'Appartement avec terrasse'],
+['title' => 'Studio centre ville']
+];
+
     /**
-     * @Route("/property", name="property")
+     * @Route("/property/{page}", name="property_list", requirements = {"page" = "\d+"})
      * Page qui liste les annonces immobilières
      */
-    public function index(): Response
+    public function index($page = 1): Response
     {
         // Pour démarrer, on va créer un tableau d'annonces
 
-        $properties = ['Maison avec piscine',
-                      'Appartement avec terrasse',
-                        'Studio centre ville'];
+        $properties = $this->properties;
 
+        dump($page);
+        // Equivalent au var_dump
         dump($properties);
+
+
 
         return $this->render('property/index.html.twig', ['properties' => $properties]);
     }
 
     /**
-     * @Route("property/maison", name="property_show")
-     * Page qui affiche une annonce
+     * @Route("property/{slug}", name="property_show")
+     * Page qui affiche une annonce avec un paramètre dynamique
      */
-    public function show() : Response {
-        return $this->render('property/show.html.twig');
+    public function show($slug) : Response {
+
+        //ici, on peut vérifier que le slug soit dans notre tableau
+
+        if (!in_array($slug, array_column($this->properties, 'title'))) {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->render('property/show.html.twig', ['slug' => $slug]);
     }
 }
