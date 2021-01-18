8a33f977d105a5c90c6eac20292baf6d325795ec
Marko1212 <shihonage7@gmail.com>
2021-01-18 15:33:43 +0100

    'update'

--- src/Form/RealEstateType.php
+++ src/Form/RealEstateType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\RealEstate;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,10 +16,30 @@ class RealEstateType extends AbstractType
         $builder
             ->add('title')
             ->add('description')
-            ->add('surface')
+            ->add('surface', RangeType::class, ['attr' => [
+                'min' => 10,
+                'max' => 400,
+                'class' => 'p-0',
+            ],
+                ])
             ->add('price')
-            ->add('rooms')
-            ->add('type')
+            // on peut chnager le type du champ avec le 2nd paramètre
+            ->add('rooms', ChoiceType::class, [
+                'choices' => [
+                    'Studio' => 1,
+                    'T2' => 2,
+                    'T3' => 3,
+                    'T4' => 4,
+                    'T5' => 5,
+                ],
+            ])
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Maison' => 'maison',
+                    'Appartement' => 'appartement'
+                ],
+                'expanded' => true
+            ])
             ->add('sold')
         ;
     }
