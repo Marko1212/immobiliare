064ff45f8a494614980aa8308a57970288298292
Marko1212 <shihonage7@gmail.com>
2021-01-19 18:17:13 +0100

    update

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,20 +5,29 @@ namespace App\DataFixtures;
 use App\Entity\RealEstate;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = Factory::create('fr:FR');
         for($i=1; $i <=100; $i++) {
             $realEstate = new RealEstate();
-            $realEstate->setTitle('Un appartement FIXTURES');
-            $realEstate->setDescription('Une description');
-            $realEstate->setSurface('10');
-            $realEstate->setPrice(100000);
-            $realEstate->setRooms(4);
-            $realEstate->setType('appartement');
-            $realEstate->setSold(false);
+            $type = $faker->randomElement(['maison', 'appartement']);
+            $title = ucfirst($type).' ';
+            $rooms = $faker->numberBetween(1, 5);
+            //dump($rooms);
+            $title .= RealEstate::SIZES[$rooms]; // T2, T3, T4
+            // Appartement (avec jardin, avec balcon)
+            // Maison T4 (en centre-ville ou en campagne)
+            $realEstate->setTitle($title);
+            $realEstate->setDescription($faker->text(2000));
+            $realEstate->setSurface($faker->numberBetween(10, 400));
+            $realEstate->setPrice($faker->numberBetween(34875, 584725));
+            $realEstate->setRooms($rooms);
+            $realEstate->setType($type);
+            $realEstate->setSold($faker->boolean(10)); // 10% de chances d'avoir true
             $manager->persist($realEstate);
 
         }
