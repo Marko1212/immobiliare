9f6a2a8d1a3bad7bbc4624d924f2857483a0d6b2
Marko1212 <shihonage7@gmail.com>
2021-01-21 17:09:39 +0100

    update

--- public/js/search.js
+++ public/js/search.js
@@ -5,5 +5,11 @@ $("#search").keyup(function() {
 
     $.ajax('/api/search/' + value, { type: 'GET' }).then(function(response) {
         console.log(response);
+        let ul = $('<ul></ul>');
+        for (let property of response.results) {
+            let li = $('<li>'+property.title+'</li>')
+            ul.append(li);
+        }
+        $("#real-estate-list").html(ul);
     });
 });
