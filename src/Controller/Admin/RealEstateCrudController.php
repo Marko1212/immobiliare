92650bb69e42717f6d6c11ac96b0df4f8718ab03
Marko1212 <shihonage7@gmail.com>
2021-01-26 09:59:36 +0100

    back office

--- src/Controller/Admin/RealEstateCrudController.php
+++ src/Controller/Admin/RealEstateCrudController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\RealEstate;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 
 class RealEstateCrudController extends AbstractCrudController
 {
@@ -12,14 +14,13 @@ class RealEstateCrudController extends AbstractCrudController
         return RealEstate::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+        return  [
+            'title', 'surface', MoneyField:: new('price')->setCurrency('EUR'), 'rooms', 'sold',
+            ImageField::new('image')->setBasePath('img/uploads')->setUploadDir('public/img/uploads'),
         ];
     }
-    */
+
 }
